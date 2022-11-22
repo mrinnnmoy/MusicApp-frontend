667dae210b588957d7e38fe2b41aa1fd35fccfa0
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-22 18:49:26 +0530

    add img-files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
-import Header from './Components/Header';
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div>
       <Header />
+      {/* <Hero />
+      <Experience />
+      <Search />
+      <Download />
+      <Footer /> */}
     </div>
   );
 }
