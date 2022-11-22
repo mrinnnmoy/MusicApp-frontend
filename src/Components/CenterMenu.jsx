9f298ec237914119e9fd38e6c70ca2fb1f6cf9ac
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-22 18:59:59 +0530

    build CenterMenu-div

---
+++ src/Components/CenterMenu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function CenterMenu() {
+  return (
+    <div className="menu flex">
+      <ul className="flex w-[100%] justify-between">
+        <li>Home</li>
+        <li>About</li>
+        <li>Preformer</li>
+        <li>Event Schedule</li>
+      </ul>
+    </div>
+  )
+};
+
+export default CenterMenu;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-// import CenterMenu from "./CenterMenu";
+import CenterMenu from './CenterMenu';
 
 function Header() {
 
   return (
     <div className="header bg-[#081730] flex items-center justify-between px-[5rem] pt-[2.4rem] text-[0.8rem]">
-      <img src="" alt="logo" className="logo w-[42px] h-[42px]" />
-      {/* <CenterMenu /> */}
+      {/* logo */}
+      <img src={require("../img/MuzicLogo.png")} alt="logo" className="logo w-[42px] h-[42px]" />
+
+      {/* side menu */}
+      <CenterMenu />
 
       <div className="buttons flex">
         <button className="mr-[35px] hover:bg-[#232A4E]">
