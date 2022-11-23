66eaed31b6c4d1bf687ac5ecc18bb5845825029b
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-23 09:19:14 +0530

    build feature-section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
+import Experience from './Components/Experience';
 
 function App() {
   return (
     <div className="App text-white overflow-hidden">
       <Header />
       <Hero />
-      {/*
       <Experience />
+
+      {/*
       <Search />
       <Download />
       <Footer /> */}
